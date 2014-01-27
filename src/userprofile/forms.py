2a3e55fe0a3a3e2493848f3513d5d1c94b7115af
Denis Geary Lopez <dgearylopez@gmail.com>
2014-01-27 00:40:17 -0500

    fixed minor positioning

--- src/userprofile/forms.py
+++ src/userprofile/forms.py
@@ -40,14 +40,14 @@ class CreateUserForm(UserCreationForm): #may also be forms.ModelForm
 		)
 
 
-   	def clean_password2(self):
+	def clean_password2(self):
 		password1 = self.cleaned_data.get("password1")
 		password2 = self.cleaned_data.get("password2")
 		if password1 and password2 and password1 != password2:
 			raise forms.ValidationError(
                 self.error_messages['password_mismatch'],
                 code='password_mismatch',
-            )
+			)
 		return password2
 
 	def save(self, commit=True):
