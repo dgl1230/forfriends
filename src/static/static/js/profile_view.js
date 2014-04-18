0f7ded238e9fc1139405882cef19467c8f73273b
Nick Kebbas <nick@nick-VirtualBox.(none)>
2014-04-17 18:26:09 -0700

    small style changes

--- src/static/static/js/profile_view.js
+++ src/static/static/js/profile_view.js
@@ -1,18 +1,5 @@
 /* Top Menu */
 $(document).ready(
-  function () {
-    $('.nav li').hover(
-      function () { 
-        $('.nav li ul li').css('display', 'inline');
-	$('ul', this).fadeIn("fast");
-      },
-      function () { 
-        $('ul', this).fadeOut("fast");
-      }
-    );
-  }
-);
-
  $(function() {
 $( "#tabs" ).tabs({
 event: "mouseover"
