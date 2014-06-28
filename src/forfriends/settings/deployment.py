ef1751e63a215baa19b49da7126b875269a6a358
dgl1230 <dgearylopez@gmail.com>
2014-06-28 12:00:08 -0700

    media url stuff

--- src/forfriends/settings/deployment.py
+++ src/forfriends/settings/deployment.py
@@ -171,6 +171,7 @@ if not DEBUG:
 
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
+    MEDIA_URL = S3_URL + '/profiles'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
     
