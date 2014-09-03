71afe44a8410751628890625ce5dc30baea86b54
dgl1230 <dgearylopez@gmail.com>
2014-09-03 10:35:41 -0700

    trying to fix piple facebook profile pic error take 10

--- src/forfriends/settings/deployment.py
+++ src/forfriends/settings/deployment.py
@@ -182,7 +182,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details'
-    'pipeline.save_profile_picture',
+    'settings.pipeline.save_profile_picture',
 )
 
 
