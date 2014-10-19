d330428c0553b265dbe51f79849ff519d62bd4d5
dgl1230 <dgearylopez@gmail.com>
2014-10-19 10:33:40 -0700

    debug turned off, local testing equals false

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -32,7 +32,7 @@ from questions.models import Question, UserAnswer
 
 
 
-CURRENTLY_LOCALLY_TESTING = True
+CURRENTLY_LOCALLY_TESTING = False
 
 
 '''Implements the 'add friend' button when viewing a user's profile
