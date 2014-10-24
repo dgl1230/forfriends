d1529ea059faadc31ac084c61d4d46b554c3a58e
dgl1230 <dgearylopez@gmail.com>
2014-10-24 13:01:11 -0700

    uncommenting get fb profile pic 15

--- src/forfriends/pipeline.py
+++ src/forfriends/pipeline.py
@@ -7,35 +7,39 @@ from social.pipeline.user import get_username as social_get_username
 
 from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+
+
 from profiles.models import Address, Job, Info, UserPicture, Gamification
 
 
 
 def save_profile_picture(strategy, user, response, details, is_new=False,*args,**kwargs):
-    try: 
-        if strategy.backend.name == 'facebook':
-            url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+    if strategy.backend.name == 'facebook':
+        url = 'http://graph.facebook.com/{0}/picture'.format(response['id'])
+        avatar = urlopen(url)
 
-            try:
-                response = request('GET', url, params={'type': 'large'})
-                response.raise_for_status()
-            except HTTPError:
-                pass
-            else:
-                picture, created = UserPicture.objects.create(user=user)
-                picture.image = ('{0}_social.jpg'.format(user.username), ContentFile(response.content))
-                picture.save()
-                '''
-                profile = user.get_profile()
-                profile.image = ('{0}_social.jpg'.format(user.username), ContentFile(response.content))
-                profile.save
-                '''            '''
-                picture, created = UserPicture.objects.get_or_create(user=user, image=ContentFile(response.content))
-                picture.is_profile_pic = True
-                picture.save()
-                '''
-    except: 
-        pass
+        try:
+            response = request('GET', url, params={'type': 'large'})
+            response.raise_for_status()
+        except HTTPError:
+            pass
+        try:
+            picture = UserPicture.objects.create(user=user)
+            #picture.image = ('{0}_social.jpg'.format(user.username), ContentFile(response.content))
+            picture.image = slugify(user.username + " social") + '.jpg', ContentFile(avatar.read())
+            picture.save()
+        except: 
+            pass
+            '''
+            profile = user.get_profile()
+            profile.image = ('{0}_social.jpg'.format(user.username), ContentFile(response.content))
+            profile.save
+            '''            '''
+            picture, created = UserPicture.objects.get_or_create(user=user, image=ContentFile(response.content))
+            picture.is_profile_pic = True
+            picture.save()
+            '''
     return
 
 
