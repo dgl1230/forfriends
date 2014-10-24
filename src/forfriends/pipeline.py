64ea053dcc29526dec130b41fb279d2b5a330268
dgl1230 <dgearylopez@gmail.com>
2014-10-24 11:58:53 -0700

    uncommenting get fb profile pic 4

--- src/forfriends/pipeline.py
+++ src/forfriends/pipeline.py
@@ -22,9 +22,14 @@ def save_profile_picture(strategy, user, response, details, is_new=False,*args,*
         except HTTPError:
             pass
         else:
+            profile = user.get_profile()
+            profile.profile_photo.save('{0}_social.jpg'.format(user.username), ContentFile(response.content))
+            profile.save()
+            '''
             picture, created = UserPicture.objects.get_or_create(user=user, image=ContentFile(response.content))
             picture.is_profile_pic = True
             picture.save()
+            '''
 
 
 
