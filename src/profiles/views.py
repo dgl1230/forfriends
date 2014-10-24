b7ea54de94f88d6b26bb7861917722c7c50530aa
dgl1230 <dgearylopez@gmail.com>
2014-10-23 18:11:59 -0700

    testing if reset circle decorator is being stupid 3

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -473,7 +473,7 @@ def generate_circle(request):
 			#messages.success(request, "We're sorry, but there aren't many users nearby you right now. We rested your circle as best we could, but you can reset it again if you'd like.")
 	return HttpResponseRedirect(reverse('home'))
 
-@user_passes_test(user_not_new, login_url=reverse_lazy('new_user_info'))
+#@user_passes_test(user_not_new, login_url=reverse_lazy('new_user_info'))
 #@user_passes_test(user_can_reset_circle, login_url=reverse_lazy('home'))
 def circle_distance(logged_in_user, preferred_distance):
 	user_gamification = Gamification.objects.get(user=logged_in_user)
