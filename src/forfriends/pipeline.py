9702bbabb3accf33b56ae17925cc16b2892e109b
dgl1230 <dgearylopez@gmail.com>
2014-10-25 14:02:18 -0700

    trying photo for type 15

--- src/forfriends/pipeline.py
+++ src/forfriends/pipeline.py
@@ -20,9 +20,9 @@ from profiles.models import Address, Job, Info, UserPicture, Gamification
 
 def save_profile_picture(strategy, user, response, details, is_new=False,*args,**kwargs):
     if strategy.backend.name == 'facebook':
-        url = 'http://graph.facebook.com/{0}/picture?width=9999&type=large&height=9999'.format(response['id'])
+        url = 'http://graph.facebook.com/{0}/picture?width=9999&height=9999'.format(response['id'])
         try:
-            response = request('GET', url, params={'type': 'large'})
+            response = request('GET', url)
             response.raise_for_status()
         except HTTPError:
             pass
