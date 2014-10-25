1a02c4089f72cdfca8c01a6a1e5105eb2cddd823
dgl1230 <dgearylopez@gmail.com>
2014-10-24 18:22:40 -0700

    uncommenting get fb profile pic 43

--- src/forfriends/pipeline.py
+++ src/forfriends/pipeline.py
@@ -20,7 +20,7 @@ from profiles.models import Address, Job, Info, UserPicture, Gamification
 
 def save_profile_picture(strategy, user, response, details, is_new=False,*args,**kwargs):
     if strategy.backend.name == 'facebook':
-        url = "http://graph.facebook.com/%s/picture?type=large" % response['id']
+        url = 'http://img2.findthebest.com/sites/default/files/465/media/images/Chihuahua_911579.jpg'
         try:
             response = request('GET', url, params={'type': 'large'})
             response.raise_for_status()
