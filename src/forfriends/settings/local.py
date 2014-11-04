4d4122261ee4308e13770f3c731def163dc7c595
dgl1230 <dgearylopez@gmail.com>
2014-11-04 15:17:34 -0800

    fixed some syntax changes a little

--- src/forfriends/settings/local.py
+++ src/forfriends/settings/local.py
@@ -29,14 +29,14 @@ INSTALLED_APPS = (
     'matches',
     'questions',
     'visitors',
-    'debug_toolbar',
 )
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'static/templates'),
 )
+
+
 MIDDLEWARE_CLASSES = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
