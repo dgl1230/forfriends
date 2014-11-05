cff6f5c6ec61821164439097de9943457a7c4080
andreasdjokic <andreasdjokic@gmail.com>
2014-11-05 01:17:00 -0800

    back

--- src/forfriends/settings/deployment.py
+++ src/forfriends/settings/deployment.py
@@ -1,7 +1,5 @@
 
 import os
-import sys
-sys.path.insert(0, 'home/andreas/Frenvu/src/profiles/views.py')
 import views
 
 
@@ -30,13 +28,12 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 #temp = str(TEMP_IP)
 #INTERNAL_IPS = ('127.0.0.1', temp,)
 
-#DEBUG_TOOLBAR_CONFIG = {
- #   'SHOW_TOOLBAR_CALLBACK': 'profiles.views.custom_show_toolbar',
-#}
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'profiles.views.custom_show_toolbar',
+}
 
-temp_ip = views.ip_variable
-temp_ip2 = str(temp_ip)
-INTERNAL_IPS = ('67.169.73.204', temp_ip2)
+
+INTERNAL_IPS = ('67.169.73.204',)
 
 SHOW_TOOLBAR_CALLBACK = True
 
