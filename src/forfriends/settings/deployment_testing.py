c7cd012c535be177275b87df65e1d377e9dc3b0c
dgl1230 <dgearylopez@gmail.com>
2014-11-05 13:26:18 -0800

    trying to get toolbar templates and sql working

--- src/forfriends/settings/deployment_testing.py
+++ src/forfriends/settings/deployment_testing.py
@@ -239,7 +239,27 @@ def get_cache():
 CACHES = get_cache()
 
 
-#LOGIN_URL = reverse_lazy('home')
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+
+
+STATICFILES_DIRS = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'static/static')
+
+
 
 
 
