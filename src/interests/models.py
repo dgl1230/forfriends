a1d8302c11723d56be07148fe1fe586bff7e0d90
dgl1230 <dgearylopez@gmail.com>
2014-11-05 12:25:09 -0800

    pushing interest db changes

--- src/interests/models.py
+++ src/interests/models.py
@@ -9,17 +9,14 @@ class Interest(models.Model):
 	interest = models.CharField(max_length=120)
 	approved = models.BooleanField(default=False)
 
-	
 	def __unicode__(self):
 		return self.interest
 
 
 
 class UserInterestAnswer(models.Model):
-
 	user = models.ForeignKey(User)
 	interest = models.ForeignKey(Interest)
-
 
 	def __unicode__(self):
 		return self.interest.interest
