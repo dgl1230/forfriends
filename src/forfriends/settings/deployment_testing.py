b0ad8423534e1c26ae885ddc336c13b7986eddb3
dgl1230 <dgearylopez@gmail.com>
2014-11-05 14:46:51 -0800

    trying to get sql part of toolbar working 3

--- src/forfriends/settings/deployment_testing.py
+++ src/forfriends/settings/deployment_testing.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 
 )
 
@@ -241,24 +241,6 @@ CACHES = get_cache()
 
 
 
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-]
-
-
-STATICFILES_DIRS = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'static/static')
 
 
 
