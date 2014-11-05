45c78b0292d282f4e4b057b2eb21b059da9fe840
dgl1230 <dgearylopez@gmail.com>
2014-11-05 14:41:06 -0800

    trying to get sql part of toolbar working 2

--- src/forfriends/settings/deployment_testing.py
+++ src/forfriends/settings/deployment_testing.py
@@ -68,14 +68,14 @@ TEMPLATE_DIRS = (
 
 
 MIDDLEWARE_CLASSES = (
-    
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    
 
 )
 
