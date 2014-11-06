914058b9eeec61d6ef6cc39a337c3e4d35bc34ba
dgl1230 <dgearylopez@gmail.com>
2014-11-06 14:17:34 -0800

    trying interests in line

--- src/interests/admin.py
+++ src/interests/admin.py
@@ -3,15 +3,24 @@ from django.contrib import admin
 from .models import Interest,  UserInterestAnswer, Category
 
 
+
+'''
 class InterestAdmin(admin.ModelAdmin):
 	class Meta:
 		model = Interest
 
 admin.site.register(Interest, InterestAdmin)
+'''
+
+
+
+class InterestInline(admin.TabularInline):	
+	model = Interest
 
 
 
 class InterestCategoryAdmin(admin.ModelAdmin):
+	inlines = [InterestInline]
 	class Meta:
 		model = Category
 
