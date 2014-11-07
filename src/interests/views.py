53e269548f32ba305c844fbaaad695d7d415bded
dgl1230 <dgearylopez@gmail.com>
2014-11-06 16:21:12 -0800

    testing gender form

--- src/interests/views.py
+++ src/interests/views.py
@@ -15,7 +15,7 @@ from django.core.cache import cache
 from profiles.views import user_not_new
 
 
-
+@user_passes_test(user_not_new, login_url=reverse_lazy('new_user_info'))
 def all_interests_experimental(request):
 	games = Interest.objects.filter(category__title='Games')
 	fitness_sports = Interest.objects.filter(category__title='Fitness/Sports')
