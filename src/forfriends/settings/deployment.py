41e02ac6b909eaa7e8fb3c66284b7ffe2a101498
dgl1230 <dgearylopez@gmail.com>
2014-11-10 14:39:55 -0800

    trying to get collect static working

--- src/forfriends/settings/deployment.py
+++ src/forfriends/settings/deployment.py
@@ -52,7 +52,6 @@ TEMPLATE_DIRS = (
 
 
 MIDDLEWARE_CLASSES = (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
