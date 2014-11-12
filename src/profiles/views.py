d97957f753bd97dcb0f8725a1001776c61e4fbf3
dgl1230 <dgearylopez@gmail.com>
2014-11-12 13:49:00 -0800

    starting to make circle generate faster 2

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -535,7 +535,7 @@ def first_circle(logged_in_user):
 				match.distance = 10000000
 			if match.distance <= 20:
 				match.percent = match_percentage(logged_in_user, single_user)
-				if match.percent >=70:
+				#if match.percent >=70:
 		i += 1
 
 			match.save()
