c14a6f2f7d003cf12c4b2bd66f437b259634ce6c
Nick Kebbas <nick@nick-VirtualBox.(none)>
2014-11-13 20:21:32 -0800

    finish interests

--- src/static/static/js/interests/like_button.js
+++ src/static/static/js/interests/like_button.js
@@ -8,20 +8,56 @@ $('.interests_form_1').ajaxForm(function() {
 $('.btn-lg').click(function(){
 	if($(this).val() == "Like it!") {
 	$(this).val("Unlike");
+	liked +=1;
+	if (liked >= 10){
+	$('.btn-lg').attr('disabled','disabled')
+	}
+	else{
+		$('.btn-lg').removeAttr('disabled');
+		$('.liked').removeAttr('disabled');		
+	}
 	}
 	else {
 		$(this).val("Like it!");
+		liked -=1;
+		if (liked >= 10){
+		$('.btn-lg').attr('disabled','disabled')
+		}
+		else{
+			$('.btn-lg').removeAttr('disabled');
+			$('.liked').removeAttr('disabled');		
+		}
 	}
 	$(this).toggleClass('liked');
 	$(this).toggleClass('btn-lg');
+	$('.liked').removeAttr('disabled');	
 });
+
+
 $('.liked').click(function(){
 	if($(this).val() == "Like it!") {
 	$(this).val("Unlike");
+	liked +=1;
+	if (liked >= 10){
+	$('.btn-lg').attr('disabled','disabled')
+	}
+	else{
+		$('.btn-lg').removeAttr('disabled');
+		$('.liked').removeAttr('disabled');		
+	}
 	}
 	else {
 		$(this).val("Like it!");
+		liked -=1;
+		if (liked >= 10){
+		$('.btn-lg').attr('disabled','disabled')
+		}
+		else{
+			$('.btn-lg').removeAttr('disabled')	
+			$('.liked').removeAttr('disabled');	
+		}
 	}
 	$(this).toggleClass('btn-lg');
 	$(this).toggleClass('liked');
+	$('.liked').removeAttr('disabled');
 });
