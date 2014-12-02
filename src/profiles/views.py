943cfc2d5f91adc6d4d692fb45919d27578df49e
dgl1230 <dgearylopez@gmail.com>
2014-12-01 20:18:26 -0800

    trying to remove the @.com part for temp username

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1104,6 +1104,7 @@ def register_new_user(request):
 		messages.success(request, 'Please provide a valid email address')
 		return render_to_response('home.html', locals(), context_instance=RequestContext(request))
 
+	email_as_username = email.split('@')[0]
 	email_as_username = email.translate(None, " '?.!/;:@#$%^&(),[]{}`~-_=+*|<>")
 	if len(email_as_username) == 0:
 		messages.error("Please provide a valid email")
