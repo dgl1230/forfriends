6dfe3bc7c5182d99ea5ec6b554c35556b3865113
dgl1230 <dgearylopez@gmail.com>
2014-12-10 00:17:29 -0800

    fixing reverse redirect error 4

--- src/forfriends/pipeline.py
+++ src/forfriends/pipeline.py
@@ -55,7 +55,8 @@ def facebook_basic_data(strategy, user, response, is_new=False, *args, **kwargs)
             info.gender = 'Male'
         user.save()
         info.save()
-    return HttpResponseRedirect(reverse('new_user_fb_or_goog', kwargs={'email': email}))
+        return HttpResponseRedirect(reverse('new_user_fb_or_goog', kwargs={'email': email}))
+    return 
 
 
 def associate_user_by_email(**kwargs):
