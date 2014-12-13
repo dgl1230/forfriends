58849e27c02cae58c68437a1253e1580d104b07f
dgl1230 <dgearylopez@gmail.com>
2014-12-12 17:14:37 -0800

    deployment settings

--- src/questions/admin.py
+++ src/questions/admin.py
@@ -12,3 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
 	class Meta:
 		model = Question
 admin.site.register(Question, QuestionAdmin)
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+	class Meta:
+		model = UserAnswer
+admin.site.register(UserAnswer, UserAnswerAdmin)
